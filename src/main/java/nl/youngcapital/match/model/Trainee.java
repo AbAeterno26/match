75fb26c6a2868dd1b9e3deab44ffe878c3e19c07
amarborz <amarborz>
2023-10-18 16:12:40 +0200

    Added Trainee class

---
+++ src/main/java/nl/youngcapital/match/model/Trainee.java
@@ -0,0 +1,118 @@
+package nl.youngcapital.match.model;
+
+public class Trainee {
+
+	private String name;
+	private String photo;
+	private String[] certificates;
+	private String birthdate;
+	private String specialty;
+	private String education;
+	private String residence;
+	private String[] skills;
+	private String availability;
+	private String email;
+	private String phonenumber;
+	private String motivation;
+	private String selfDescription;
+	private String currentTask;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	public String[] getCertificates() {
+		return certificates;
+	}
+	public void setCertificates(String[] certificates) {
+		this.certificates = certificates;
+	}
+	
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	
+	public String getSpecialty() {
+		return specialty;
+	}
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+	
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	
+	public String getResidence() {
+		return residence;
+	}
+	public void setResidence(String residence) {
+		this.residence = residence;
+	}
+	
+	public String[] getSkills() {
+		return skills;
+	}
+	public void setSkills(String[] skills) {
+		this.skills = skills;
+	}
+	
+	public String getAvailability() {
+		return availability;
+	}
+	public void setAvailability(String availability) {
+		this.availability = availability;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	
+	public String getMotivation() {
+		return motivation;
+	}
+	public void setMotivation(String motivation) {
+		this.motivation = motivation;
+	}
+	
+	public String getSelfDescription() {
+		return selfDescription;
+	}
+	public void setSelfDescription(String selfDescription) {
+		this.selfDescription = selfDescription;
+	}
+	
+	public String getCurrentTask() {
+		return currentTask;
+	}
+	public void setCurrentTask(String currentTask) {
+		this.currentTask = currentTask;
+	}
+	
+}
